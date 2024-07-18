a490b5dcda555fb1124e30d11ca7c78ca28c006f
Ashley <ash.193@live.com>
2024-07-17 22:57:21 -0400

    Adding functionality

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    setTodos(newTodoList);
+  }
+
+  function handleDeleteTodos(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index;
+    });
+    setTodos(newTodoList);
+  }
+  function handleEditTodos(index) {}
   return (
-    <main>
-      <TodoInput />
-      <TodoList />
-    </main>
+    <>
+      <TodoInput handleAddTodos={handleAddTodos} />
+      <TodoList handleDeleteTodos todos={todos} />
+    </>
   );
 }
 
--- reactjs-todolist/src/components/TodoCard.jsx
+++ reactjs-todolist/src/components/TodoCard.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
-export default function TodoCard() {
-  return <div></div>;
+export default function TodoCard(props) {
+  const { children, handleDeleteTodo } = props;
+  return (
+    <li className="todoItem">
+      {children}
+      <div className="actionsContainer">
+        <button>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button
+          onClick={() => {
+            handleDeleteTodo();
+          }}
+        >
+          <i className="fa-regular fa-trash-can"></i>
+        </button>
+      </div>
+    </li>
+  );
 }
